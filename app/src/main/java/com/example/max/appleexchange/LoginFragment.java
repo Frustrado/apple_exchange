669f38b3034dcc49d4fe98b46f2c199e0a6a713c
Worldmaster <maksymiliankrysa@gmail.com>
2019-03-20 19:05:58 +0100

    Login Fragment Design - End

--- app/src/main/java/com/example/max/appleexchange/LoginFragment.java
+++ app/src/main/java/com/example/max/appleexchange/LoginFragment.java
@@ -13,5 +13,6 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login,container,false);
+        
     }
 }
