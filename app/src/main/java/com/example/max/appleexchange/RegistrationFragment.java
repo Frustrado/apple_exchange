d9c96ea8401ef18f77d94207da4f8ef39bb68f04
Worldmaster <maksymiliankrysa@gmail.com>
2019-03-25 01:55:39 +0100

    + Scroll View to Register Fragment + Connection with Firebase + Addition basic createUser with email and password (FirebaseAuth)

--- app/src/main/java/com/example/max/appleexchange/RegistrationFragment.java
+++ app/src/main/java/com/example/max/appleexchange/RegistrationFragment.java
@@ -5,21 +5,74 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
+import static android.content.ContentValues.TAG;
 
 public class RegistrationFragment extends Fragment {
-    private MainActivity mainActivity;
-    private Toolbar toolbar;
-
-
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private Button register;
+    private ProgressBar progressBar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        // return inflater.inflate(R.layout.fragment_login,container,false);
         View v = inflater.inflate(R.layout.fragment_registration,container,false);
+        email= v.findViewById(R.id.editTextEmail);
+        password=v.findViewById(R.id.editTextPassword);
+        register=v.findViewById(R.id.button_singin);
+        mAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(email.getText().toString(),password.getText().toString());
+            }
+        });
+
         return v;
+    }
+
+    public void createAccount(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+                            //Toast.makeText(getContext(), "Authentication failed.",
+                                    //Toast.LENGTH_SHORT);
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 }
